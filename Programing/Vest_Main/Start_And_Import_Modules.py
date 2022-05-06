419cebe447a220184000c232514776112b2af3d0
Jimmytheprofessionaldumbass <103012079+Jimmytheprofessionaldumbass@users.noreply.github.com>
2022-05-06 12:19:33 +0800

    Create Start_And_Import_Modules.py

---
+++ Programing/Vest_Main/Start_And_Import_Modules.py
@@ -0,0 +1,28 @@
+print("Starting")
+
+'''This indicates to the user that the program/code 
+is running and (hopefully) working'''
+
+import time
+'''This imports time module'''
+time.sleep(0.5)
+'''This adds a delay so the user notices'''
+print("Starting.")
+'''This indicates that the program has entered 
+the second stage of starting up'''
+
+import random
+'''This imports the random module 
+(You can add more modules if it is needed)'''
+time.sleep(0.05)
+print("Starting..")
+
+import cv2 as cv
+'''This Imports the OpenCV Module for Computer Vision'''
+time.sleep(0.05)
+print("Starting...")
+
+time.sleep(0.05)
+print("Finished Loading.")
+'''This tells the user that the program 
+has finished loading modules'''
