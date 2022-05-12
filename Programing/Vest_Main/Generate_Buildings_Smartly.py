1f3e39451b91e01a65218b804d5c461178cb0654
Jimmytheprofessionaldumbass <103012079+Jimmytheprofessionaldumbass@users.noreply.github.com>
2022-05-12 11:06:45 +0800

    Update Generate_Buildings_Smartly.py

--- Programing/Vest_Main/Generate_Buildings_Smartly.py
+++ Programing/Vest_Main/Generate_Buildings_Smartly.py
@@ -1,1 +1,1 @@
-"""This Program shuffles Programs in a smart way to make the game more fair for botht eh speedrunner and the ManHunter"""
+"""This Program shuffles Programs in a smart way to make the game more fair for both the speedrunner and the ManHunter"""
